a981df8f70e71247e9cd28def454b5c83cdec3bf
John Davis <35755411+johndavistl@users.noreply.github.com>
2018-11-10 23:20:46 +0530

    Create connection.php
    Connecting to MySQL Database

---
+++ include/connection.php
@@ -0,0 +1,19 @@
+//MySQLi Procedural
+
+<?php 
+
+$servername="localhost";
+$username="root";
+$password="";
+$dbname="InnoDB";
+
+//create connection
+$conn = mysqli_connect($servername,$username,$password);
+
+//check connection
+if (!$conn) {
+  die("Connection Failed".mysqli_connect_error());
+}
+echo "Connected Sucessfully";
+
+?>
